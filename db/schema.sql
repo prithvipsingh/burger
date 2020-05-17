742067e1537b1d0447eb8751abab46a43c5b0c6b
prithvipsingh <ppsdeora@gmail.com>
2020-05-17 14:17:31 +1000

    update schema and seed.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,11 @@
-* Create the `burgers_db`.
-   * Switch to or use the `burgers_db`.
-   * Create a `burgers` table with these fields:
-     * **id**: an auto incrementing int that serves as the primary key.
-     * **burger_name**: a string.
-     * **devoured**: a boolean.
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+id int NOT NULL AUTO_INCREMENT,
+burger_name varchar(255),
+devoured BOOLEAN default false,
+`date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (id)
+);
