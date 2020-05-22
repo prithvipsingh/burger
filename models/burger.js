37ea1fe1042260a5f924109bc47b1c2d6d85c000
prithvipsingh <ppsdeora@gmail.com>
2020-05-22 18:15:15 +1000

    create functions to get all burgers

--- models/burger.js
+++ models/burger.js
@@ -1,12 +1,14 @@
+
+// Importing orm.js (require)
 var orm = require("../config/orm");
 
-
 var burger = {
+   // function for select all burgers from table 
  selectAll: function(cb){
      orm.all("burgers", function(res){
          cb(res);
      });
- }
+ },
 
 };
 
